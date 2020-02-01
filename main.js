ccc3c98a7095fb12b94a65d3a0cf8b08eebafadb
Olamide Oladinni <olajide1987@gmail.com>
2020-02-01 00:09:15 -0600

    add update

--- main.js
+++ main.js
@@ -1,1 +1,69 @@
-console.log("Sorting Hat");
+const students = [];
+const houses = [
+  'Slytherin',
+  'Ravenclaw',
+  'Gryffindor',
+  'Hufflepuff',
+];
+
+const getRandomHouse = (max) => {
+      return Math.floor(Math.random()*Math.floor(max))
+
+}
+
+
+
+
+
+
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = textToPrint;
+}
+
+    const studentBuilder = (arrayToPrint) => {
+        let domString = '';
+        students.forEach((student)=>{
+          domString += '<div class="card col-3">';
+          domString += `<p class="name">${student.name}</p>`;
+          domString += `<p class="house">${student.house}</p>`;
+          domString += '<button class="btn btn-danger">Expel</button>';
+
+          domString += '</div>';
+        });
+          printToDom('students', domString)
+        };
+
+          const createCard = (e) => {
+            e.preventDefault();
+            const inputName = document.getElementById('inputName').value;
+            const newName = {
+              name:inputName,
+              house:houses[getRandomHouse(4)]
+              // id:`student${counter}`
+            }
+            students.push(newName);
+            // counter++;
+            studentBuilder(students);
+            inputName = ' ';
+          }
+
+          // displayForm function that calls the element form by id and uses the method to remove the class hide 
+          const displayForm = () => {
+            const form = document.getElementById('form');
+            form.classList.toggle('hide');
+          }
+          // event listener function that calls the method by id and uses the method event listener
+          const eventListener = () => {
+            document.getElementById('sorting').addEventListener('click',displayForm);
+            document.getElementById('addStudentBtn').addEventListener('click',createCard);
+
+          }
+          //initial function that contains all functions 
+          const init = () => {
+            eventListener();
+          }
+          //call initial function
+          init();
+
+          
